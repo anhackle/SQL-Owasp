58d50650c8f6d944d857305604e13d4b23fbdfe8
anhackle <nguyencaothai.vn@gmail.com>
2022-07-21 08:44:00 +0700

    Bug code

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mysql = require('mysql');
-const cookieParser = require('cookie-parser');
 
 const app = express();
+app.disable('etag');
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "nguyencaothai123#",
     database: "secret_db"
 });
 
@@ -19,69 +18,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cookieParser());
-app.disable('etag'); // Disable 304 status code.
-
-function randomStr(len, arr) {
-    var ans = '';
-    for (var i = len; i > 0; i--) {
-        ans += arr[Math.floor(Math.random() * arr.length)];
-    }
-    return ans;
-}
-
-function checkingTrackingid(trackingId, callback) {
-    query = `select * from secret_db.trackedusers where trackingid = ?;`;
-    con.query(query, [trackingId],function (err, results) {
-        callback(true);
-    });
-}
 
 app.get("/", function (req, res) {
     query = `select * from secret_db.items where released=1`;
-    con.query(query, function (err, results) {
-        if (err) {
+    con.query(query, function(err,  results) {
+        if (err){
             res.send(err.sqlMessage);
-        } else {
-            cookieClient = req.cookies.trackingid;
-            if (!cookieClient) {
-                cookieValue = randomStr(20, 'abcdefgABCDEG1234567890');
-
-                // Add trackingid into table
-                query = `insert into secret_db.trackedusers(trackingid) values('${cookieValue}');`
-                con.query(query, function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-
-                res.cookie('trackingid', cookieValue, {
-                    "path": "/",
-                    "httpOnly": true,
-                });
-            }
-            checkingTrackingid(req.cookies.trackingid, (visited) => {
-                res.render("start", {
-                    results: results
-                });
-            });
+        }else{
+            res.render("start", {results:results});
         }
     });
 });
 
-app.get("/filter", function (req, res) {
+app.get("/filter", function(req,res){
     var category = req.query.category;
-    query = `select * from secret_db.items where type= ? and released=1;`
+    query = `select * from secret_db.items where type= '${category}' and released=1;`
     console.log(query);
-    con.query(query,[category], function (err, results) {
-        if (err) {
+    con.query(query,function(err, results){
+        if (err){
             res.send(err.sqlMessage);
-        } else {
-            checkingTrackingid(req.cookies.trackingid, (visited) => {
-                res.render("start", {
-                    results: results
-                });
-            });
+        }else{
+            res.render("start", {results:results});
         }
     });
 });
@@ -89,9 +46,7 @@ app.get("/filter", function (req, res) {
 
 app.get("/login", function (req, res) {
     var login_failed = 1;
-    res.render("login", {
-        login_failed: login_failed
-    });
+    res.render("login", {login_failed:login_failed});
 });
 
 app.get("/register", function (req, res) {
@@ -100,25 +55,23 @@ app.get("/register", function (req, res) {
 
 app.get("/secrets", function (req, res) {
     res.render("secrets");
-})
+}) 
 
 app.post("/register", function (req, res) {});
 
 app.post("/login", function (req, res) {
     username = req.body.username;
     password = req.body.password;
-    query = `select * from secret_db.users where username= ? and password= ? ;`;
-    con.query(query, [username,password],function (error, results) {
-        if (error) {
+    query = `select * from secret_db.users where username='${username}' and password='${password}';`;
+    con.query(query, function(error, results){
+        if (error){
             res.send(error.sqlMessage);
-        } else {
-            if (results.length > 0) {
+        }else{
+            if (results.length > 0){
                 res.render("secrets");
-            } else {
+            }else{
                 var login_failed = 0;
-                res.render("login", {
-                    login_failed: login_failed
-                });
+                res.render("login", {login_failed:login_failed});
             }
         }
     });
